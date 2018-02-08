8438220ed04ec93f8a64be024ae9e4d97b397068
PilotCo <cody.pilot@outlook.com>
2018-02-07 17:20:05 -0700

    correcting an issue with the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+//var methodOverride = require('method-override');
 
 //set up the express app
 var app = express();
